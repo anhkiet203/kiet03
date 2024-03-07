86d576db6251d06348511c7269687b1756339f6c
anhkiet2030 <dinhanhkiet2003@gmail.com>
2024-03-07 16:42:20 +0700

    git commit

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Lyen Lyen';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng 8/3';
+const titleCard = 'Tặng bà nè';
+const contentCard = 'Chúc bà ngày 8/3 vui vẻ nha, ngày càng xinh đẹp và thành công hơn trong cuộc sống sau này nha và gặp được người luôn yêu thương bà nè!';
+
+
+const giftImage = 'em.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/jpg;base64, " + base64;
